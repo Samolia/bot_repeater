71ad7889578da292a7b29e230521feec717bc72c
samolia <samolia9@gmail.com>
2022-04-08 17:01:26 +0300

    refactoring

--- bot_repeater.py
+++ bot_repeater.py
@@ -10,11 +10,12 @@ client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 async def handler_new_message(event):
     try:
         await client.send_message(TARGET_CHANNEL, event.message)
+        print('I sended message')
 
     except Exception as e:
         print(e)
 
 if __name__ == '__main__':
-    print("I work bitch!")
+    print("I work!")
     client.start()
     client.run_until_disconnected()
